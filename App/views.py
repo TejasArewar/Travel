6bf47d496f4fb89fe968958c4ed11aff79d48d7f
TejasArewar <104343599+TejasArewar@users.noreply.github.com>
2026-02-25 21:54:25 +0530

    ok

--- App/views.py
+++ App/views.py
@@ -334,4 +334,28 @@ def get_user_itineraries(request, user_id):
         'success': True,
         'itineraries': itineraries_data,
         'user_name': user.name
+
     })
+
+
+
+
+
+
+
+
+from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+
+def create_admin(request):
+    User = get_user_model()
+
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="admin",
+            email="admin@gmail.com",
+            password="Admin@123"
+        )
+        return HttpResponse("Superuser created!")
+    
+    return HttpResponse("Superuser already exists.")
